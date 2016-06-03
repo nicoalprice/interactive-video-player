94a9594d37e8edc5d30ccdc75295ae74c054748b
Nicoal Price <nicoalprice@gmail.com>
2016-06-03 15:29:35 -0400

    added comments with project instructions to the html and js files

---
+++ js/app.js
@@ -0,0 +1,34 @@
+
+
+// Implement the play and pause buttons.
+
+// Add volume button that lets you mute the sound or turn it on.
+
+// Implement the fullscreen button.
+
+/* Implement the playback progress control.
+A user should be able to click anywhere on the playback bar to jump to that part of the video.
+As the video plays the playback bar should fill in.
+As the video plays the current time should be displayed and updated e.g. 0:10 / 11:34. */
+
+/* Use Javascript or CSS to hide and show the video player button on mouse hover states.
+Only the progress bar should remain. */
+
+/* As the media playback time changes, sentences in the transcript should highlight.
+Use JavaScript to listen for those changes and apply a highlight to the appropriate sentence.
+You can use the captions.vtt file to see the times at which the words are spoken in the video. */
+
+
+// EXCEEDS EXPECTATIONS //
+
+// Embed the .vtt file as a closed captioning track and add a button to video controls to toggle captions on and off.
+
+// A creative and thoughtful responsive design.
+
+// Playback speed control or other helpful controls.
+
+// Volume control so viewer can adjust the volume level, not just mute or on.
+
+// Playback controls include buffering progress of the downloaded video.
+
+// When the user clicks on any sentence in the transcript the video player jumps to the appropriate time in the video.
