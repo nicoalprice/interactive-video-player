b55cfd473df42a7ef60435f2ea426d9c3d9074f8
Nicoal Price <nicoalprice@gmail.com>
2016-07-12 21:05:42 -0400

    adding in additional video control functions

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 // Variables
-var vid, playButton, seekBar, curTime, muteButton, volumeBar, fullScreen;
+var vid, playButton, seekBar, curTime, muteButton, volumeBar, fullScreen, captionButton;
 
 
 // Initialize video player
@@ -10,6 +10,7 @@ function initializeVideo() {
     seekBar = document.getElementById("seek-slider");
     curTime = document.getElementById("current-time");
     muteButton = document.getElementById("mute");
+    captionButton = document.getElementById("caption-button");
     volumeBar = document.getElementById("volume-bar");
     fullScreen = document.getElementById("full-screen");
 
@@ -17,6 +18,7 @@ function initializeVideo() {
     playButton.addEventListener("click", playPause, false);
     seekBar.addEventListener("change", vidSeek, false);
     vid.addEventListener("timeupdate", seekTimeUpdate, false);
+    captionButton.addEventListener("click", closedCaptions, false);
     volumeBar.addEventListener("change", volumeControl, false);
     muteButton.addEventListener("click", mute, false);
     fullScreen.addEventListener("click", screenSize, false);
@@ -110,11 +112,23 @@ You can use the captions.vtt file to see the times at which the words are spoken
 
 // Embed the .vtt file as a closed captioning track and add a button to video controls to toggle captions on and off.
 
+/*
+function closedCaptions() {
+    if (vid.something) {
+
+    }
+    else {
+
+    }
+}
+*/
+
 // A creative and thoughtful responsive design.
 
 // Playback speed control or other helpful controls.
 
 // Volume control so viewer can adjust the volume level, not just mute or on.
+//done
 
 // Playback controls include buffering progress of the downloaded video.
 
